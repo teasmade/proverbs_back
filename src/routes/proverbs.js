f47f6ea74071c0e6f647081c357a69eb65bcb786
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-07-21 20:52:22 +0200

    add post proverb route

--- src/routes/proverbs.js
+++ src/routes/proverbs.js
@@ -1,3 +1,6 @@
+// CamelCase disabled as there's a conflict between sql field and JS variable naming conventions
+// TODO: some kind of name parser to resolve this?
+/* eslint-disable camelcase */
 const express = require('express');
 const connection = require('../../db-config');
 
@@ -5,6 +8,7 @@ const router = express.Router({
   mergeParams: true,
 });
 
+// get all proverbs
 router.get('/', (req, res) => {
   const sql = 'SELECT * FROM proverb';
   connection.query(sql, (dbErr, dbResults) => {
@@ -15,6 +19,34 @@ router.get('/', (req, res) => {
       res.status(200).json(dbResults);
     }
   });
+});
+
+// create one proverb
+router.post('/', (req, res) => {
+  const { orig_lang, proverb_text, proverb_date, proverb_author } = req.body;
+  console.log(orig_lang);
+  const sql =
+    'INSERT INTO proverb(orig_lang, proverb_text, proverb_date, proverb_author) VALUES (?, ?, ?, ?)';
+  connection.query(
+    sql,
+    [orig_lang, proverb_text, proverb_date, proverb_author],
+    (dbErr, dbResults) => {
+      if (dbErr) {
+        console.error(dbErr);
+        res.status(500).send('Error saving proverb');
+      } else {
+        const id = dbResults.insertId;
+        const createdProverb = {
+          id,
+          orig_lang,
+          proverb_text,
+          proverb_date,
+          proverb_author,
+        };
+        res.status(201).send(createdProverb);
+      }
+    }
+  );
 });
 
 module.exports = router;
