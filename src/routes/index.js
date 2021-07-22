a1a96cdb44d1591b8805a8e38e65030306938f4b
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-07-22 09:11:38 +0200

    add route for one proverb translations

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 // import routes
 const proverb = require('./proverb');
 const proverbs = require('./proverbs');
+const proverbtranslations = require('./proverbtranslations');
 
 // use routes
 const router = express.Router();
 router.use('/proverb', proverb);
 router.use('/proverbs', proverbs);
+router.use('/proverbtranslations', proverbtranslations);
 
 module.exports = router;
